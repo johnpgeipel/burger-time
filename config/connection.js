3a630a9b317d91700bb605decd4e9d4d20422572
Johnny Rocksalt <9876543+johnpgeipel@users.noreply.github.com>
2019-07-14 22:28:28 -0500

    still having heroku issues

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,43 @@
+// var mysql = require("mysql");
+// var connection;
+
+// if(process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: "root",
+//     password: "password",
+//     database: "burgers_db"
+//   });
+// };
+
+
+
+//   connection.connect();  
+//   module.exports = connection;
 var mysql = require("mysql");
 var connection;
 
-if(process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+// uses JawsDB to configure connection
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
-    host: 'localhost',
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-  });
-};
+	connection = mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: "password",
+		database: "burgers_db"
+	});
+}
 
+connection.connect(function(err) {
+	if (err) {
+		console.log("error connecting: " + err.stack);
+		return;
+	}
 
+	console.log("connected as id: " + connection.threadId);
+});
 
-  connection.connect();  
-  module.exports = connection;
+module.exports = connection;
