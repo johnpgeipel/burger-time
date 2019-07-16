722645cc1204401618f5369f7b116ddaf9e255b4
Johnny Rocksalt <9876543+johnpgeipel@users.noreply.github.com>
2019-07-16 10:47:03 -0500

    fixing heroku

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ USE burgers_db;
 
 CREATE TABLE burgers(
 	id INT AUTO_INCREMENT NOT NULL ,
-	burger_name VARCHAR(255) NOT NULL,
+	burger_name VARCHAR(50) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
 	createdAt TIMESTAMP NOT NULL,
 	PRIMARY KEY(id)
